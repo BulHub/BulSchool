0ef9882008525d76d61e3715d951fb1db3dbf0aa
Bilalov_Bulat <bulat-cloud-1@yandex.ru>
2020-04-07 17:22:04 +0300

    Thanks to the fucking annotations @CreatedDate and @LastModifiedDate for working!

--- BulSchool/src/main/java/ru/itis/models/BaseEntity.java
+++ BulSchool/src/main/java/ru/itis/models/BaseEntity.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -11,6 +12,7 @@ import java.util.Date;
 @MappedSuperclass
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Id
@@ -18,12 +20,14 @@ public class BaseEntity {
     private Long id;
 
     @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created")
     private Date created;
 
     //FIXME: Annotations @CreatedDate and @LastModified don't work!
 
     @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated")
     private Date updated;
 
