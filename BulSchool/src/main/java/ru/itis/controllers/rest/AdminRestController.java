bd84728f127bdc0c4f9d78ed4324099b291cd95d
SuperPyperCoolPerson <bulat.bilalov2000@inbox.ru>
2020-04-11 14:12:32 +0300

    Updating "AdminRestController"

--- BulSchool/src/main/java/ru/itis/controllers/rest/AdminRestController.java
+++ BulSchool/src/main/java/ru/itis/controllers/rest/AdminRestController.java
@@ -3,13 +3,13 @@ package ru.itis.controllers.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.itis.dto.AdminUserDto;
 import ru.itis.models.User;
 import ru.itis.services.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -22,7 +22,7 @@ public class AdminRestController {
         this.userService = userService;
     }
 
-    @GetMapping(value = "users/{email}")
+    @GetMapping(value = "/users/{email:.+}")
     public ResponseEntity<AdminUserDto> getUserByEmail(@PathVariable(name = "email") String email) {
         User user = userService.find(email);
         if (user == null) {
@@ -30,6 +30,31 @@ public class AdminRestController {
         }
         AdminUserDto result = AdminUserDto.fromUser(user);
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<AdminUserDto>> getUsers() {
+        List<User> users = userService.findAll();
+        if (users.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        List<AdminUserDto> userDtoList = new ArrayList<>();
+        for (User user : users) {
+            userDtoList.add(AdminUserDto.fromUser(user));
+        }
+        return new ResponseEntity<>(userDtoList, HttpStatus.OK);
+    }
+
+    @PostMapping("/users/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        userService.add(user);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/users/delete")
+    public ResponseEntity<User> deleteUser(@RequestBody User user) {
+        userService.delete(user);
+        return ResponseEntity.ok().build();
     }
 
 }
